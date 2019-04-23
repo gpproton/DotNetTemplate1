13f54f06cfa73d0127f27d5678269baeffa3a275
Godwin peter .O <godwin@peter.com.ng>
2019-04-23 12:21:06 +0100

    Some little mod and test

--- MobileApp/App.xaml.cs
+++ MobileApp/App.xaml.cs
@@ -7,10 +7,17 @@ namespace MobileApp {
 
 		public App ()
 		{
-			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense ("");
+            String MyLicense = "OTA1NDZAMzEzNzJlMzEyZTMwWnhyMHpsdWRDdmtybFIzcjFEd2dhcitDNHlvamdZUDRsdU10QmtTOXo5RT0=;OTA1NDdAMzEzNzJlMzEyZTMwQWtmcno1ZTRoaDhLMG5JRXhERlcwSWlSQmFBc3lscTVjVytQVkI5c1l0bz0=;OTA1NDhAMzEzNzJlMzEyZTMwSnpPcG5BbUhzRWdNWGpxcDZMS1VvMlZDSm9HOHZaY0cvV3F2ZkhkUjlUMD0=;OTA1NDlAMzEzNzJlMzEyZTMwU0VJekJzWXNIYzM0bDVGMXhWZXRXVVR5OG5tOVl1cTd5YmN1Uk45VTdEaz0=;OTA1NTBAMzEzNzJlMzEyZTMwTVlqMW9MQkRTUUZIdXlOREl1SWEyNVNTYnJjNXYzRmkvK3NKSEowQWR3OD0=;OTA1NTFAMzEzNzJlMzEyZTMwTDFiWHp6TXFFUloyQUo3cDBlTC9DQi9sb2J0c1U2SWVQelVOdVhYb3ZMST0=;OTA1NTJAMzEzNzJlMzEyZTMwZmhmbDY2aXdlbjNmdzZkRmhLRU1nbWk0MXE4NnNRQ2Z4YnljWktOWTVXOD0=;OTA1NTNAMzEzNzJlMzEyZTMwYVRRbFAxcjNMdkp6T1lrZFZNNmFFUXFuOTVaa2h5Q2YzTHJtMWVhNkM0VT0=;OTA1NTRAMzEzNzJlMzEyZTMwUkprUzJGOVdvUEdmUkpYeHIvYTNZKzBUYXIvbElNUDZpOFBkNVJsMWJEYz0=;OTA1NTVAMzEzNzJlMzEyZTMwTVlqMW9MQkRTUUZIdXlOREl1SWEyNVNTYnJjNXYzRmkvK3NKSEowQWR3OD0=";
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense (MyLicense);
+
+
 			InitializeComponent ();
 
-			MainPage = new LoginPage ();
+#if DEBUG
+            HotReloader.Current.Start(this);
+#endif
+
+            MainPage = new LoginPage ();
 		}
 
 		protected override void OnStart ()
---
+++ Randoms/DbStructure.sql
@@ -0,0 +1,67 @@
+﻿# Host: localhost  (Version 5.6.37)
+# Date: 2019-04-23 10:02:37
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "category"
+#
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  `categoryid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `categoryid` (`categoryid`),
+  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`categoryid`) REFERENCES `category` (`Id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "category"
+#
+
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `Id` varchar(255) NOT NULL DEFAULT '',
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `uname` varchar(255) DEFAULT NULL,
+  `email` varchar(1024) DEFAULT NULL,
+  `password` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "users"
+#
+
+
+#
+# Structure for table "items"
+#
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `suject` varchar(1024) DEFAULT NULL,
+  `body` varchar(4000) DEFAULT NULL,
+  `categoryid` int(11) DEFAULT NULL,
+  `userid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `userid` (`userid`),
+  KEY `categoryid` (`categoryid`),
+  CONSTRAINT `items_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`Id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `items_ibfk_2` FOREIGN KEY (`categoryid`) REFERENCES `category` (`Id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "items"
+#
+
